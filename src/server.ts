83b6591efff9f887b4502495c16a1c6b4b568565
kdimsan <kdimsantana@gmail.com>
2024-01-17 19:24:25 +0000

    first commit with basic configs

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+var express = require("express");
+var cors = require("cors");
+const axios = require("axios");
+
+const routes = require("./routes");
+
+var app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(4000, function () {
+  console.log("Server is running on PORT 4000");
+});
