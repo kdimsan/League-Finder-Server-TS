3273e399ee49e3b28cfdfd3f8498c75d251d8f64
kdimsan <kdimsantana@gmail.com>
2024-02-14 17:50:25 +0000

    bug: changes for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "proxyserverts",
-      script: "./dist/src/server.js",
+      script: "./dist/server.js",
       watch: false,
       exec_mode: "fork",
       instances: 1,
